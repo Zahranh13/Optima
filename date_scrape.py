c3592ed562ff98e20c156dda6c14076508713dac
Zahranh13 <80092308+Zahranh13@users.noreply.github.com>
2021-10-05 09:48:57 +0700

    Delete date_scrape.py

--- date_scrape.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-def convert_to_date_object(date_string):
-    date_object = datetime.strptime(date_string, "%d-%m-%Y")
-    thn = int(date_object.strftime('%Y'))
-    bln = int(date_object.strftime('%m'))
-    tgl = int(date_object.strftime('%d'))
-    list = [tgl, bln, thn]
-    return list
